2d46e47fdd1c5287c6aeeb516f1805c5887661a8
hristog121 <hristog121@kth.se>
2020-01-26 19:04:26 +0100

    variable refactoring - trees

--- ID1019-KTH/trees.ex
+++ ID1019-KTH/trees.ex
@@ -2,32 +2,34 @@ defmodule Trees do
   #Find if a key is  a member of the tree
   def member(_,:nil) do :no end
 
-  def member(e, {:leaf,e}) do :yes end
+  def member(element, {:leaf,element}) do :yes end
   def member(_, {:leaf, _}) do :no end
 
-  def member(e, {:node, e, _, _}) do :yes end
-  def member(e, {:node, v, left, _}) when e < v do
-    member(e,left)
+  def member(element, {:node, element, _, _}) do :yes end
+  def member(element, {:node, value, left, _}) when element < value do
+    member(element,left)
   end
-  def member(e, {:node, _, _, right})  do
-    member(e, right)
+  def member(element, {:node, _, _, right})  do
+    member(element, right)
   end
 
   #Insert element into the tree
-  def insert(e, :nil)  do  ...  end
-  def insert(e, {:leaf, v}) when e < v  do
+  def insert(element, :nil)  do
+    ...
+  end
+  def insert(element, {:leaf, value}) when element < value  do
 
        end
 
-  def insert(e, {:leaf, v}) do
+  def insert(element, {:leaf, value}) do
 
       end
-  def insert(e, {:node, v, left, right }) when e < v do
-    {:node, e, v, insert(e,left)}
+  def insert(element, {:node, value, left, right }) when element < value do
+    {:node, element, value, insert(eelement,left)}
   end
 
-  def insert(e, {:node, v, left, right })  do
-    {:node, e, v, insert(e,right)}
+  def insert(element, {:node, value, left, right })  do
+    {:node, element, value, insert(element,right)}
   end
 
 end
